e8adba8845e22d41b9b12bea74c3cafe6064e27c
patience <patience7862@gmail.com>
2024-09-18 23:07:35 +0800

    original make

---
+++ Codes/cv_learning/origin_make/main.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main()
+{
+    // 创建一个3x3的矩阵，并显示在终端
+    cv::Mat mat = (cv::Mat_<double>(3, 3) << 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    std::cout << "Matrix: " << std::endl
+              << mat << std::endl;
+
+    return 0;
+}
+
+// g++ main.cpp -o test `pkg - config-- cflags-- libs opencv4`
+// ./test
