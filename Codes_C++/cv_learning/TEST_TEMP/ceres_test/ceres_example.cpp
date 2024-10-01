c5b718156e287b41655faabe9aff48f420a2cc50
patience <patience7862@gmail.com>
2024-10-01 23:29:01 +0800

    "finish"

--- Codes_C++/cv_learning/TEST_TEMP/ceres_test/ceres_example.cpp
+++ Codes_C++/cv_learning/TEST_TEMP/ceres_test/ceres_example.cpp
@@ -1,20 +1,101 @@
-// curve_fitting_per_frame.cpp
-#include <ceres/ceres.h>
-#include <iostream>
-#include <vector>
-#include <utility>
-#include <random>
+#include "ceres/ceres.h"
+#include "glog/logging.h"
+using ceres::AutoDiffCostFunction;
+using ceres::CostFunction;
+using ceres::Problem;
+using ceres::Solve;
+using ceres::Solver;
+// Data generated using the following octave code.
+//   randn('seed', 23497);
+//   m = 0.3;
+//   c = 0.1;
+//   x=[0:0.075:5];
+//   y = exp(m * x + c);
+//   noise = randn(size(x)) * 0.2;
+//   y_observed = y + noise;
+//   data = [x', y_observed'];
 
-// 残差块，定义线性模型的残差 y - (a * x + b)
-struct LinearCostFunctor
+const int kNumObservations = 67;
+// clang-format off
+const double data[] = {
+  0.000000e+00, 1.133898e+00,
+  7.500000e-02, 1.334902e+00,
+  1.500000e-01, 1.213546e+00,
+  2.250000e-01, 1.252016e+00,
+  3.000000e-01, 1.392265e+00,
+  3.750000e-01, 1.314458e+00,
+  4.500000e-01, 1.472541e+00,
+  5.250000e-01, 1.536218e+00,
+  6.000000e-01, 1.355679e+00,
+  6.750000e-01, 1.463566e+00,
+  7.500000e-01, 1.490201e+00,
+  8.250000e-01, 1.658699e+00,
+  9.000000e-01, 1.067574e+00,
+  9.750000e-01, 1.464629e+00,
+  1.050000e+00, 1.402653e+00,
+  1.125000e+00, 1.713141e+00,
+  1.200000e+00, 1.527021e+00,
+  1.275000e+00, 1.702632e+00,
+  1.350000e+00, 1.423899e+00,
+  1.425000e+00, 1.543078e+00,
+  1.500000e+00, 1.664015e+00,
+  1.575000e+00, 1.732484e+00,
+  1.650000e+00, 1.543296e+00,
+  1.725000e+00, 1.959523e+00,
+  1.800000e+00, 1.685132e+00,
+  1.875000e+00, 1.951791e+00,
+  1.950000e+00, 2.095346e+00,
+  2.025000e+00, 2.361460e+00,
+  2.100000e+00, 2.169119e+00,
+  2.175000e+00, 2.061745e+00,
+  2.250000e+00, 2.178641e+00,
+  2.325000e+00, 2.104346e+00,
+  2.400000e+00, 2.584470e+00,
+  2.475000e+00, 1.914158e+00,
+  2.550000e+00, 2.368375e+00,
+  2.625000e+00, 2.686125e+00,
+  2.700000e+00, 2.712395e+00,
+  2.775000e+00, 2.499511e+00,
+  2.850000e+00, 2.558897e+00,
+  2.925000e+00, 2.309154e+00,
+  3.000000e+00, 2.869503e+00,
+  3.075000e+00, 3.116645e+00,
+  3.150000e+00, 3.094907e+00,
+  3.225000e+00, 2.471759e+00,
+  3.300000e+00, 3.017131e+00,
+  3.375000e+00, 3.232381e+00,
+  3.450000e+00, 2.944596e+00,
+  3.525000e+00, 3.385343e+00,
+  3.600000e+00, 3.199826e+00,
+  3.675000e+00, 3.423039e+00,
+  3.750000e+00, 3.621552e+00,
+  3.825000e+00, 3.559255e+00,
+  3.900000e+00, 3.530713e+00,
+  3.975000e+00, 3.561766e+00,
+  4.050000e+00, 3.544574e+00,
+  4.125000e+00, 3.867945e+00,
+  4.200000e+00, 4.049776e+00,
+  4.275000e+00, 3.885601e+00,
+  4.350000e+00, 4.110505e+00,
+  4.425000e+00, 4.345320e+00,
+  4.500000e+00, 4.161241e+00,
+  4.575000e+00, 4.363407e+00,
+  4.650000e+00, 4.161576e+00,
+  4.725000e+00, 4.619728e+00,
+  4.800000e+00, 4.737410e+00,
+  4.875000e+00, 4.727863e+00,
+  4.950000e+00, 4.669206e+00,
+};
+// clang-format on
+// 第一步：定义残差结构体。
+struct ExponentialResidual
 {
-    LinearCostFunctor(double x, double y) : x_(x), y_(y) {}
+    ExponentialResidual(double x, double y) : x_(x), y_(y) {}
 
     template <typename T>
-    bool operator()(const T *const ab, T *residual) const
+    bool operator()(const T *const m, const T *const c, T *residual) const
     {
-        // ab[0] = a, ab[1] = b
-        residual[0] = T(y_) - (ab[0] * T(x_) + ab[1]);
+        residual[0] = y_ - exp(m[0] * x_ + c[0]);
         return true;
     }
 
@@ -23,78 +104,33 @@ private:
     const double y_;
 };
 
-// 生成带噪声的线性数据
-std::vector<std::pair<double, double>> GenerateNoisyData(double a, double b, int num_points, double noise_std)
+int main(int argc, char **argv)
 {
-    std::vector<std::pair<double, double>> data;
-    std::default_random_engine generator;
-    std::normal_distribution<double> distribution(0.0, noise_std);
-
-    for (int i = 0; i < num_points; ++i)
+    google::InitGoogleLogging(argv[0]);
+    // 设置初始值
+    double m = 0.0;
+    double c = 0.0;
+    // 残差加入problem，每个观测值都需要添加
+    Problem problem;
+    for (int i = 0; i < kNumObservations; ++i)
     {
-        double x = static_cast<double>(i);
-        double y = a * x + b + distribution(generator);
-        data.emplace_back(x, y);
+        problem.AddResidualBlock(
+            new AutoDiffCostFunction<ExponentialResidual, 1, 1, 1>(
+                new ExponentialResidual(data[2 * i], data[2 * i + 1])),
+            NULL,
+            &m,
+            &c);
     }
 
-    return data;
-}
+    Solver::Options options;
+    options.max_num_iterations = 25;
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = true;
 
-int main()
-{
-    // 模拟多帧数据参数
-    const int num_frames = 10;       // 总帧数
-    const int points_per_frame = 50; // 每帧的数据点数
-    const double true_a = 2.0;       // 真正的斜率
-    const double true_b = 5.0;       // 真正的截距
-    const double noise_std = 1.0;    // 噪声标准差
-
-    // 存储每一帧的拟合参数
-    std::vector<std::pair<double, double>> fitted_parameters;
-
-    // 初始参数猜测
-    double ab[2] = {0.0, 0.0}; // ab[0] = a, ab[1] = b
-
-    for (int frame = 0; frame < num_frames; ++frame)
-    {
-        // 生成当前帧的带噪声数据
-        std::vector<std::pair<double, double>> data = GenerateNoisyData(true_a, true_b, points_per_frame, noise_std);
-
-        // 构建优化问题
-        ceres::Problem problem;
-        for (const auto &point : data)
-        {
-            ceres::CostFunction *cost_function = new ceres::AutoDiffCostFunction<LinearCostFunctor, 1, 2>(
-                new LinearCostFunctor(point.first, point.second));
-            problem.AddResidualBlock(cost_function, nullptr, ab);
-        }
-
-        // 配置求解器
-        ceres::Solver::Options options;
-        options.linear_solver_type = ceres::DENSE_QR;
-        options.minimizer_progress_to_stdout = false; // 设为 true 可查看每帧的优化过程
-        options.max_num_iterations = 100;
-
-        // 求解
-        ceres::Solver::Summary summary;
-        ceres::Solve(options, &problem, &summary);
-
-        // 存储当前帧的拟合参数
-        fitted_parameters.emplace_back(ab[0], ab[1]);
-
-        // 输出当前帧的结果
-        std::cout << "Frame " << frame + 1 << ":\n";
-        std::cout << "  Estimated a: " << ab[0] << ", b: " << ab[1] << "\n";
-        std::cout << "  Summary: " << summary.BriefReport() << "\n\n";
-    }
-
-    // 输出所有帧的拟合参数
-    std::cout << "All fitted parameters per frame:\n";
-    for (size_t i = 0; i < fitted_parameters.size(); ++i)
-    {
-        std::cout << "Frame " << i + 1 << ": a = " << fitted_parameters[i].first
-                  << ", b = " << fitted_parameters[i].second << "\n";
-    }
-
+    Solver::Summary summary;
+    Solve(options, &problem, &summary);
+    std::cout << summary.BriefReport() << "\n";
+    std::cout << "Initial m: " << 0.0 << " c: " << 0.0 << "\n";
+    std::cout << "Final   m: " << m << " c: " << c << "\n";
     return 0;
 }
---
+++ Task03/第二次任务喵/task/build/CMakeFiles/task.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/patience/Documents/VSCode/Repos/XJTU-RMV-Task03/第二次任务喵/task/main.cpp" "/home/patience/Documents/VSCode/Repos/XJTU-RMV-Task03/第二次任务喵/task/build/CMakeFiles/task.dir/main.cpp.o"
+  "/home/patience/Documents/VSCode/Repos/XJTU-RMV-Task03/第二次任务喵/task/src/windmill.cpp" "/home/patience/Documents/VSCode/Repos/XJTU-RMV-Task03/第二次任务喵/task/build/CMakeFiles/task.dir/src/windmill.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "/usr/include/eigen3"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
