9c96e21e43234979479cb02129e20b99a66eb5b3
patience <patience7862@gmail.com>
2024-10-01 18:07:29 +0800

    "ready again"

--- XJTU-RMV-Task03/第二次任务喵/task/main.cpp
+++ XJTU-RMV-Task03/第二次任务喵/task/main.cpp
@@ -12,7 +12,7 @@ int main()
     while (1)
     {
         t = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
-        src = wm.getMat((double)t.count() / 1000);
+        src = wm.getMat((double)t.count()); // Is here wrong? (why to divide 1000?) Still, it can run well. But it is unreasonal.
         /*code*/
         // starttime
         int64 start_time = getTickCount();
@@ -74,24 +74,25 @@ int main()
             }
         }
 
-        // locate
+        // locate & draw
         Moments M_R = moments(contours[R_id]);
-        int R_x = int(M_R.m10 / M_R.m00);
-        int R_y = int(M_R.m01 / M_R.m00);
-        // circle(src, Point(R_x, R_y), 4, Scalar(255, 0, 0), -1); // draw
+        Point R_xy(int(M_R.m10 / M_R.m00), int(M_R.m01 / M_R.m00));
+        // circle(src, R_xy, 4, Scalar(255, 0, 0), -1); // draw
 
         Moments M_ham = moments(contours[hammer_id]);
-        int ham_x = int(M_ham.m10 / M_ham.m00);
-        int ham_y = int(M_ham.m01 / M_ham.m00);
-        // circle(src, Point(ham_x, ham_y), 4, Scalar(255, 0, 0), -1); // draw
+        Point ham_xy(int(M_ham.m10 / M_ham.m00), int(M_ham.m01 / M_ham.m00));
+        // circle(src, ham_xy, 4, Scalar(255, 0, 0), -1); // draw
+
+        // calculate vector RH & cos(angle_now)
+        Point RH = ham_xy - R_xy;
+        Point2d RHi = Point2d(RH) / norm(RH);
 
         // ceres
-        
 
         // endtime
         int64 end_time = getTickCount();
         double t = (end_time - start_time) / getTickFrequency();
-        cout << t << endl;
+        // cout << t << endl;
         /*code*/
         imshow("windmill", src);
         waitKey(1);
